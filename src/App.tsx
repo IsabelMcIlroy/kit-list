d77af219a440c22782d48b47db40402d9e29ed42
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2023-03-20 07:15:55 -0230

    tailwind, project setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
 import styles from './App.module.css';
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
+      <h1 class="text-3xl font-bold underline text-red-600 dark:text-purple-300">polar bear</h1>
     </div>
   );
 };
