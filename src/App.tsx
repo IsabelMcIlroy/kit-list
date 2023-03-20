3e777675fce4f75dfc05d3990c84f48e5bd1160d
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2023-03-20 08:08:57 -0230

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import type { Component } from 'solid-js';
+import netlifyIdentity from 'netlify-identity-widget';
 
 import styles from './App.module.css';
+
+netlifyIdentity.init({
+  //container: '#netlify-modal', // defaults to document.body
+  locale: 'en' // defaults to 'en'
+});
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <h1 class="text-3xl font-bold underline text-red-600 dark:text-purple-300">polar bear</h1>
+      <button onClick={()=>{console.log(netlifyIdentity)
+      netlifyIdentity.open()}}>Login</button>
     </div>
   );
 };
