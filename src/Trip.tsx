40407b12922b61e86fd2996eafa4b25e8572b4a8
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2023-05-27 11:58:35 -0230

    Create Trip.tsx

---
+++ src/Trip.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const Trip: Component = () => {
+  return (
+    <div class="bg-light-gray">
+        Hello
+    </div>
+  );
+};
+
+export default Trip;
